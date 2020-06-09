14cef31df02a0cc68c63a0c1a2ea2dbbbaa68f55
eliy <eliy@sqreamtech.com>
2020-06-09 10:12:12 +0300

    setup.py fix

--- setup.py
+++ setup.py
@@ -20,8 +20,6 @@ setup_params = dict(
     python_requires =  '>=3.6',
     install_requires = ['sqlalchemy>=1.3.17',
                        'pysqream>=3.0.3'],
-    package_dir =      {'': 'pysqream_sqlalchemy'},
-    
     entry_points =     {
         'sqlalchemy.dialects':
             ['sqream = pysqream_sqlalchemy.dialect:SqreamDialect']
