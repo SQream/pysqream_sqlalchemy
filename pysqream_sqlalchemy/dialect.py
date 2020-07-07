58094a05c36f89ec73139410bb4aed6c629f8302
EliYk <32999848+EliYk@users.noreply.github.com>
2020-07-07 16:45:24 +0300

    case sensitivity fix when using select get_ddl

--- pysqream_sqlalchemy/dialect.py
+++ pysqream_sqlalchemy/dialect.py
@@ -265,7 +265,7 @@ class SqreamDialect(DefaultDialect):
         ''' Used by SQLAlchemy's Table() which is called by Superset's get_table()
             when trying to add a new table to the sources'''
 
-        query = "select get_ddl('{}')".format(table_name)
+        query = f'select get_ddl(\'"{table_name}"\')'
         res = connection.execute(query).fetchall()
         table_ddl = ''.join(tup[0] for tup in res).splitlines()
 
