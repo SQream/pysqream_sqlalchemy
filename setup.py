2b9682694e9bf2a5448de2bf148458b5cf1c9114
EliYk <32999848+EliYk@users.noreply.github.com>
2020-08-17 14:24:08 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup_params = dict(
     
     name =             'pysqream_sqlalchemy',
-    version =          '0.2',
+    version =          '0.3',
     description =      'SQLAlchemy dialect for SQreamDB', 
     long_description = open("README.rst", "r").read() + '\n\n',
     url=               "https://github.com/SQream/pysqream_sqlalchemy",
