f1ec2459beac223699b894b545b8df42cf3d7398
danielg-sqream <100589834+danielg-sqream@users.noreply.github.com>
2022-10-19 11:21:57 +0300

    Update setup.py
    change dependency from 3.1.8 pysqream version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup_params = dict(
     python_requires =  '>=3.6',
     
     install_requires = ['sqlalchemy>=1.3.18',
-                        'pysqream>=3.1.9',
+                        'pysqream>=3.1.8',
                         'setuptools==57.4.0',
                         'pytest==6.2.3',
                         'pudb==2022.1.2',
